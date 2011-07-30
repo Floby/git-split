6c9286bacbeb360c8b5381daf573abea99c4694d
Florent Jaby <florent.jaby@gmail.com>
2011-07-31 00:07:44 +0200

    fix die function for exit code

--- lib/util.sh
+++ lib/util.sh
@@ -1,10 +1,18 @@
+# quit the program with an error message
+# usage: die <code> <message>
 die() {
-    code=""
-    [ -z "$1" ] && code=1
+    code=1
+    if [[ "$1" =~ ^[0-9]+$ ]]; then
+        code=$1
+        shift
+    fi
     log $*
-    exit $1 $code
+    exit $code
 }
 
+# echo on stderr
 log() {
     echo $* 1>&2
 }
+
+
