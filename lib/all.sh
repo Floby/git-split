d2bd6730adc28e43f284120dd6ff22500a0bd279
Florent Jaby <florent.jaby@gmail.com>
2011-07-31 00:23:33 +0200

    add functions object_exists and object_type to all.sh

--- lib/all.sh
+++ lib/all.sh
@@ -1,3 +1,15 @@
 for file in `ls $SPLIT_LIB_DIR | grep -v all.sh` ; do
     source $SPLIT_LIB_DIR/$file
 done
+
+
+object_exists() {
+    git cat-file -e $1
+    return $?
+}
+
+object_type() {
+    object_exists $1 || die 3 "object does not exist $1"
+    git cat-file -t $1
+    return $?
+}
