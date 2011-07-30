418cfe204f1a395cf0c294dd38bc476e5f40501f
Florent Jaby <florent.jaby@gmail.com>
2011-07-31 00:36:09 +0200

    move listing function into commit.sh

--- lib/commit.sh
+++ lib/commit.sh
@@ -15,3 +15,32 @@ get_subpath_tree() {
     tree=$(git cat-file -p $commit | head -1 | sed 's/^tree //')
     find_subtree $tree $path
 }
+
+# list commits that affect a given subpath from a given branch
+# usage: list_useful_commits <subpath> <branch>
+#       subpath:    the subpath for which you want to scan changes
+#       branch:     not used yet
+list_useful_commits() {
+    subpath=$1
+    branch=HEAD
+
+    # intialize these
+    current_tree=`get_subpath_tree $branch`
+    previous_commit=`deref_commit $branch`
+    for commit in `git rev-list $branch` ; do
+        subtree=`get_subpath_tree $commit $subpath`
+
+        if [ "$current_tree" = "$subtree" ] ; then
+            previous_commit=$commit
+            continue
+        fi
+        current_tree=$subtree
+
+        log keeping commit $previous_commit
+        git cat-file -p $previous_commit | sed -n '/^$/,$p' | tail -n +2
+        echo $previous_commit >> $commit_list
+        log
+
+        previous_commit=$commit
+    done
+}
